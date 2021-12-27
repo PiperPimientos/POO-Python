3db8aff618646294c2ba445f581afb01cad2a587
felipe <feliperestrepomeneses@gmail.com>
2021-12-27 12:42:50 -0500

    agregamos decomposition, con un ejemplo de un Automovil y como funciona su motor

--- decomposition.py
+++ decomposition.py
@@ -1,12 +1,25 @@
 class Automovil:
 
+    # def __init__(self, modelo, marca, color):
+    #     self.modelo = modelo
+    #     self.marca = marca
+    #     self.color = color
+    #     self._estado = 'en reposo'
+    #     self._motor = None
+
     def __init__(self, modelo, marca, color):
         self.modelo = modelo
         self.marca = marca
         self.color = color
         self._estado = 'en reposo'
-        self._motor = None
-    
+        self._motor = Motor(cilindros=4)
+
+    #Metodo para la aceleracion del Automovil
+    def acelerar(self, tipo='despacio'):
+        if tipo == 'rapida':
+            self._motor_inyecta_gasolina(10)
+        else:
+            self._motor._inyecta_gasolina(3)    
 
 class Motor:
 
